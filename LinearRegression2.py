1f04a355069fa029f431defefeeb167ec237b02f
Reezo912 <clizaga@gmail.com>
2025-08-02 18:30:17 +0200

    x

--- LinearRegression2.py
+++ LinearRegression2.py
@@ -4,9 +4,14 @@ import numpy as np
 # w: pesos
 # b: bias
 
-''' Conociendo esto, implementare la regresion lineal basica multivariante ytilizando la
- libreria NumPy'''
+'''
+En este proyecto, implementare la regresion lineal basica multivariante utilizando la
+libreria NumPy
 
+ Para esto utilizaré la funcion de descenso de gradiente para minimizar la funcion de error,
+ que en este caso será MSE(Error cuadrático medio)
+
+ '''
 class LinealRegresion():
     def __init__(self, epoch=1000, lr=0.001) -> None:
         self.lr = lr
@@ -16,4 +21,20 @@ class LinealRegresion():
 
     def fit(self, X, y):
         n_samples, n_features = X.shape
-        
+        self.w = np.zeros(n_features)
+        self.b: float = 0.0
+
+        for epoch in range(self.epoch):
+            '''
+            Por cada Epoch:
+            - Defino mi funcion de la regresion lineal
+            - Calculo la matriz de error.
+            - Calculo el MSE.
+            '''
+            y_pred = self.w @ X + self.b
+            error = y - y_pred
+            loss = np.mean(error**2)
+
+            # Aplico el descenso de gradiente
+            dw = -(2/smaple)
+
