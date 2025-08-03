Refactor LinearRegression.py to improve method signatures with type hints, fix MSE calculation, and remove unnecessary data fetching and preprocessing code. Update test_linear.py to reflect changes and add TODO for pipeline implementation to prevent data leakage.

src/LinearRegression.py
@@ -1,19 +1,4 @@
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import numpy as np
-
-cali_data = fetch_california_housing()
-
-X = cali_data.data
-y = cali_data.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-scaler = StandardScaler()
-
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.fit_transform(X_test)
 
 
 # Formula y_pred = X_test @ w + b
@@ -36,7 +21,7 @@ class LinearRegression():
         self.w = None  # el valor es None dado que no conozco las dimensiones de X_test
         self.b: float = 0.0
 
-    def fit(self, X, y):
+    def fit(self, X, y)-> None:
         n_samples, n_features = X.shape
         self.w = np.zeros(n_features)
         self.b: float = 0.0
@@ -64,15 +49,15 @@ class LinearRegression():
             if epoch % 1000 == 0:
                 print(f'Epoch {epoch}: Loss = {loss:.6f}')
 
-    def predict(self, X):
+    def predict(self, X)-> np.ndarray:
         '''
         Funcion de prediccion de resultados
         '''
         return X @ self.w + self.b
 
-    def evaluate_MSE(self, X, y):
+    def evaluate_mse(self, X, y)-> np.float64:
         '''
         Funcion evaluacion de resultados con MSE
         '''
         y_pred = self.predict(X)
-        return np.mean((y - y_pred**2))
+        return np.mean((y - y_pred)**2)

tests/test_linear.py
@@ -9,27 +9,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 
-# Cargar datos
+
+# TODO: introducir Pipelines para evitar dataleakage en el escalado
+
+
 cali_data = fetch_california_housing()
 X, y = cali_data.data, cali_data.target
 
-# Preprocesamiento
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Modelo sklearn
 model_sk = LinearRegression()
 model_sk.fit(X_train, y_train)
 y_pred_sk = model_sk.predict(X_test)
 
-# Tu modelo
 model = MyLinearRegression(lr=0.0005, epoch=100000)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-# Métricas
+
 mse_train = np.mean((y_train - model.predict(X_train))**2)
 mse_test = np.mean((y_test - y_pred)**2)
 mse_test_sk = np.mean((y_test - y_pred_sk)**2)